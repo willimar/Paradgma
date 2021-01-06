b2fc4acb963b39deaf1770adf6217a24c19adf75
Willimar Augusto Rocha <willimar@gmail.com>
2021-01-06 15:20:53 -0300

    Task one implementation

---
+++ src/Pradigma/PaisFilhos.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pradigma
+{
+    public class Entity
+    {
+        public string Name { get; set; }
+        public List<Entity> Child { get; set; }
+
+        public Entity(string name)
+        {
+            this.Name = name;
+            this.Child = new List<Entity>();
+        }
+    }
+
+    public static class ThereIsEntity
+    {
+        public static bool ThereIs(this Entity entity, string name)
+        {
+            var result = entity.GetPlace(name);
+
+            return result != null;
+        }
+
+        public static Entity GetPlace(this Entity entity, string name)
+        {
+            if (entity.Name.ToLower().Equals(name.ToLower()))
+            {
+                return entity;
+            }
+
+            Entity result = null;
+
+            foreach (var child in entity.Child)
+            {
+                result = child.GetPlace(name);
+
+                if (result != null)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    #region Exceptions
+
+    internal class ThereIsException : Exception
+    {
+        public ThereIsException() : base("E2: Some item found in tree.")
+        {
+        }
+    }
+
+    internal class PlaceNotFoundException : Exception
+    {
+        public PlaceNotFoundException(string name) : base($"E4: The Entity with name '{name}' was not found.")
+        {
+        }
+    }
+
+    internal class ChildRangeException : Exception
+    {
+        public ChildRangeException(string entityName) : base($"The Entity '{entityName}' has two child.")
+        {
+        }
+    }
+
+    internal class RootWasFoundException : Exception
+    {
+        public RootWasFoundException(string name) : base($"E3: There is a root place with name '{name}'.")
+        {
+        }
+    }
+
+    #endregion
+
+    internal class Solution
+    {
+        private Entity _entity;
+
+        public Solution(string name)
+        {
+            this._entity = new Entity(name);
+        }
+
+        public void AddItem(string name, string place)
+        {
+            if (this._entity.Name.ToLower().Equals(name.ToLower()))
+            {
+                throw new RootWasFoundException(name);
+            }
+
+            if (this._entity.ThereIs(name))
+            {
+                throw new ThereIsException();
+            }
+
+            var entityPlace = this._entity.GetPlace(place) ?? throw new PlaceNotFoundException(place);
+
+            if (entityPlace.Child.Count() == 2)
+            { 
+                throw new ChildRangeException(entityPlace.Name);
+            }
+
+            entityPlace.Child.Add(new Entity(name));
+        }
+    }
+
+    
+}
---
+++ src/Pradigma/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Pradigma
+{
+    class Program
+    {
+        static void Wellcome()
+        {
+            var line = new string('-', 100);
+            var space1 = new string(' ', 44);
+            Console.Clear();
+            Console.WriteLine(line);
+            Console.WriteLine($"|{space1} WELLCOME {space1}|");
+            Console.WriteLine(line);
+        }
+
+        static Solution GetSolution()
+        {
+            Console.Write("Enter with root name: ");
+            var root = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(root))
+            {
+                throw new ArgumentException("E4: Invalid value to root.");
+            }
+
+            return new Solution(root);
+        }
+
+        static Solution solution = null;
+
+        static void Main(string[] args)
+        {
+            while (solution == null)
+            {
+                Wellcome();
+
+                try
+                {
+                    solution = GetSolution();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Press the Key to continue.");
+                    Console.ReadKey();
+                }
+            }
+
+            Console.WriteLine("Write exit to finish;");
+            var name = string.Empty;
+
+            while (!name.ToLower().Equals("exit"))
+            {
+                try
+                {
+                    Console.Write("Value name: ");
+                    name = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(name))
+                    {
+                        throw new ArgumentException("E4: Invalid value to value name");
+                    }
+
+                    if (!name.ToLower().Equals("exit"))
+                    {
+                        Console.Write("Place name: ");
+                        var place = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(place))
+                        {
+                            throw new ArgumentException("E4: Invalid value to place name");
+                        }
+
+                        solution.AddItem(name, place);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
